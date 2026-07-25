1ad7f54156e19ea8327f71415a22e80000a4420d
ai-engineer06h1 <ai.engineer06h1@gmail.com>
2026-07-25 16:21:35 +0530

    Generated from idea: a simple todo app where users add, check off, and delete tasks

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LandingScreen from './components/LandingScreen';
+
+function App() {
+  return (
+    <div>
+      <LandingScreen />
+    </div>
+  );
+}
+
+export default App;
